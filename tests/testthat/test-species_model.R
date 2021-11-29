627cac6b612ab2b5bd01d1859f647d99b4985711
Philip Mostert <philism@dhcp-10-24-23-26.wlan.ntnu.no>
2021-11-29 10:28:48 +0100

    Added test that for species_model

---
+++ tests/testthat/test-species_model.R
@@ -0,0 +1,102 @@
+testthat::test_that('Test that species_model can produce all of the returns correctly.', {
+
+  ##Create arbitrary data
+  #Arbitrary projection
+  projection <- CRS('+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
+
+  ##Run species_model
+
+  #Check return = 'boundary'
+
+  boundary <- species_model(return = 'boundary')
+  expect_true(class(boundary)[1] == 'SpatialPolygons')
+
+  #Check different boundary
+
+  boundaryHedmark <- species_model(return = 'boundary', location = 'Hedmark')
+  expect_true(class(boundaryHedmark)[1] == 'SpatialPolygonsDataFrame')
+  expect_true(boundaryHedmark$NAME_1 == 'Hedmark')
+
+  #Check an error
+
+  expect_error(species_model(return = 'boundary', location = 'Sweden'),
+               'At least one of the locations provided is not a valid county in Norway. NOTE: Trøndelag is given as Nord-Trøndelag and Sør-Trøndelag')
+
+
+  #Check return = 'mesh'
+  #make wide mesh
+
+  mesh <- species_model(return = 'mesh', boundary = boundary,
+                        meshParameters =  list(cutoff=0.08, max.edge=c(1, 3), offset=c(1,1)))
+
+  expect_true(class(mesh) == 'inla.mesh')
+
+  meshPlot <- species_model(return = 'mesh plot', boundary = boundary,
+                            meshParameters =  list(cutoff=1, max.edge=c(1, 3), offset=c(1,1)))
+
+  expect_setequal(class(meshPlot), c('gg', 'ggplot'))
+
+  ##Make species
+
+  #Make random points
+  #Random presence only dataset
+  #Choose 10 points of arbitrary species
+  species <- 'Fraxinus excelsior'
+
+  PO <- spsample(boundary, n = 20, 'random', CRSobs = projection)
+  #Add species name
+  PO$species <- sample(x = species, size = nrow(PO@coords), replace = TRUE)
+
+  #Random presence absence dataset
+  PA <- spsample(boundary, n = 20, 'random', CRSobs = projection)
+  #Add species name
+  PA$species <- sample(x = species, size = nrow(PA@coords), replace = TRUE)
+  #Add response name
+  PA$PAresp <- sample(x = c(0,1), size = nrow(PA@coords), replace = TRUE)
+  #Add trial name
+  PA$trials <- sample(x = c(1,2,3), size = nrow(PA@coords), replace = TRUE)
+
+  dataObj <- structured_data(PO, PA, datasetType = c('PO','PA'), responsePA = 'PAresp',
+                             trialsPA = 'trials', speciesName = 'species', coordinateNames = c('x','y'))
+
+
+  #Check return = 'species plot'
+
+  speciesPlot <- species_model(return = 'species plot', structuredData = dataObj,
+                               boundary = boundary, speciesNames = species,
+                               limit = 10, mesh = mesh)
+
+  expect_setequal(class(speciesPlot), c('gg', 'ggplot'))
+
+  #Check return = 'model'
+
+  covariate <- 'Annual Mean Temperature'
+  model <- species_model(return = 'model',
+                         boundary = boundary, speciesNames = species,
+                         limit = 10, mesh = mesh, worldclimCovariates = covariate)
+
+  expect_setequal(class(model), c('bru_sdm', 'bru', 'iinla', 'inla'))
+
+
+  #Check return = 'predictions'
+
+  species <- c(species, 'Ulmus glabra')
+
+  predictions <- species_model(return = 'predictions',
+                               boundary = boundary, speciesNames = species,
+                               limit = 10, mesh = mesh, worldclimCovariates = covariate)
+
+  expect_setequal(class(predictions), c("predict_bru_sdm", "list"))
+
+  #Check return = 'predictions map'
+
+  predictions <- species_model(return = 'predictions map',
+                               boundary = boundary, speciesNames = species,
+                               limit = 10, mesh = mesh, worldclimCovariates = covariate)
+
+  expect_setequal(class(predictions), c('gg', 'ggplot'))
+
+
+
+
+})
