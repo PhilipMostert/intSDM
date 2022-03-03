52f543fee0eb040611eda7177f3dc694d9dfc4d8
Philip Mostert <philism@Philips-MacBook-Pro.local>
2022-03-03 09:24:18 +0100

    Changes to testthat for structured_data

--- tests/testthat/test-structured_data.R
+++ tests/testthat/test-structured_data.R
@@ -29,7 +29,7 @@ testthat::test_that('Test that structured_data is able to differentiate between 
   dataObj <- structured_data(PO, PA, datasetType = c('PO','PA'), responsePA = 'PAresp',
                             trialsPA = 'trials', speciesName = 'species', coordinateNames = c('x','y'))
 
-  expect_s4_class(dataObj, 'structured_data')
+  expect_s4_class(dataObj, 'structuredData')
 
   expect_true(class(dataObj@dataPO$PO) == 'SpatialPointsDataFrame')
   expect_true(class(dataObj@dataPA$PA) == 'SpatialPointsDataFrame')
@@ -43,7 +43,7 @@ testthat::test_that('Test that structured_data is able to differentiate between 
   ##Check warnings
 
   expect_error(structured_data(PO, PA, datasetType = c('Present only','PA'), responsePA = 'PAresp',
-                                trialsPA = 'trials', speciesName = 'species', coordinateNames = c('x','y')),'datasetType must be a vector with values: "PO" or "PA".')
+                                trialsPA = 'trials', speciesName = 'species', coordinateNames = c('x','y')),'datasetType must be a vector with values: "count", PO" or "PA".')
   PA_wrong_coords <- PA
   colnames(PA_wrong_coords@coords) <- c('NotX','NotY')
   expect_error(structured_data(PO, PA_wrong_coords, datasetType = c('PO','PA'), responsePA = 'PAresp',
