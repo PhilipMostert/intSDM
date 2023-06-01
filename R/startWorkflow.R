9cae91d7a97f536b897803c0a9e8c4cf441ac09a
Philip Mostert <philip.s.mostert@ntnu.no>
2023-06-01 11:40:39 +0200

    First commit: startWorkflow

--- R/startWorkflow.R
+++ R/startWorkflow.R
@@ -1,14 +1,14 @@
 #' @description
-#' Function to initialize the reproducible workflow using integrated species distribution models.
-#' @param Countries A vector of country names to complete the analysis over. If missing, a boundary object may be added to the model using \code{.$addArea}.
-#' @param Species A vector of Species names (scientific) to include in the analysis.
-#' @param Save Logical argument indicating if the model objects and outputs should be saved. Defaults to \code{TRUE}.
-#' @param saveOptions A list containing two items: \code{projectDirectory} indicating where the objects should be saved, and \code{projectName} which indicates the name for the folder in the relevant directory.
+#' Function to initialize the reproducible workflow using integrated species distribution models. The arguments for this function are used to specify which species and countries are to be studied, as well as how the results of the model should be saved (either as an R object or saved to some directory). This function outputs an \code{R6} object with additional slot functions to help further customize the model specification. See \code{?species_model} for more details on these functions.
+#' @param Countries A vector of country names to complete the analysis over. If missing, a boundary object (of class \code{Spatial} or \code{sf}) has to be added to the model using \code{.$addArea} before any analysis is completed.
+#' @param Species A vector of Species names (scientific) to include in the analysis. Names should be given carefully since the names provided will be used to obtain _GBIF_ observations.
+#' @param Save Logical argument indicating if the model objects and outputs should be saved as .rds files. Defaults to \code{TRUE}. If \code{FALSE} then the output of the workflow will be a list of objects at each step of the workflow.
+#' @param saveOptions A list containing two items: \code{projectDirectory} indicating where the objects should be saved (defaults to \code{getwd()}), and \code{projectName} which indicates the name for the folder in the relevant directory. The latter argument is required, regardless of the value given to \code{Save}.
 #' @param Quiet Logical argument indicating if the workflow should provide the user messages during the setup and estimation process. Defaults to \code{TRUE}.
 #'
 #' @returns An R6 object of class \code{species_model}. This object contains a collection of slot functions to assist the user in customizing their workflow.
 #'
-#'
+#' @export
 
 startWorkflow <- function(Countries, Species,
                           Projection,
@@ -16,7 +16,7 @@ startWorkflow <- function(Countries, Species,
                           saveOptions = list(
                           projectDirectory = getwd(),
                           projectName = NULL),
-                          Quiet = TRUE) {
+                          Quiet = FALSE) {
 
   if (Save) {
 
@@ -26,9 +26,9 @@ startWorkflow <- function(Countries, Species,
 
   if (missing(Species)) stop('At least one species name needs to be provided.')
 
-  if(!inherits(Projection, 'CRS')) stop('Projection needs to be a CRS object.')
+  #if(!inherits(Projection, 'CRS')) stop('Projection needs to be a CRS object.')
 
-  if (Quiet) {
+  if (!Quiet) {
 
   cat('Initializing workflow for integrated species distribution model:\n\n')
 
@@ -72,21 +72,24 @@ print.data.frame(descriptionSlots, right = FALSE, row.names = FALSE)
 
 cat('\nThe workflow may then be estimated using the "sdmWorkflow" function.\n\n')
 
-if (Save) {
 
-dir.create(path = paste0(saveOptions$projectDirectory, '/', saveOptions$projectName))
 
-cat('Directory for model outputs is:\n', paste0(saveOptions$projectDirectory, '/', saveOptions$projectName))
+  }
 
-}
+  if (Save) {
 
-}
+    dir.create(path = paste0(saveOptions$projectDirectory, '/', saveOptions$projectName))
+
+    if (!Quiet) cat('Directory for model outputs is:\n', paste0(saveOptions$projectDirectory, '/', saveOptions$projectName))
+
+  }
 
   modelSetup <- species_model$new(Countries = Countries,
                                   Species = Species,
                                   Projection,
+                                  Save = Save,
                                   Quiet = Quiet,
                                   nameProject = saveOptions$projectName,
-                                  Directory = saveOptions$projectDirectory)
+                                  Directory = paste0(saveOptions$projectDirectory, '/', saveOptions$projectName))
 
 }
