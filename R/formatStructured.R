d525ad9ccbc657feee75998a2b770300b55ca65c
Philip Mostert <philip.s.mostert@ntnu.no>
2023-06-01 11:38:46 +0200

    First commit: formatStructured

--- R/formatStructured.R
+++ R/formatStructured.R
@@ -1,36 +1,51 @@
+#' @description
+#' Function used to format structure data into a coherent framework.
+#'
+#' @param dataOcc The species occurrence data. May be either a \code{SpatialPointsDataFrame}, \code{sf} or \code{data.frame} object.
+#' @param type The type of observation model for the data. May be either: \code{PO}, \code{PA} or \code{Counts}.
+#' @param varsOld The names of the old variables in the model which need to be converted to something new.
+#' @param varsNew The name of the new variables in the model which will be used in the full model.
+#' @param projection The CRS object to add to the species occurrence data.
+#' @param boundary An sf object of the boundary of the study area, used to check if the data points are over the region.
+#'
+#' @return An \code{sf} object containing the locations of the species.
 
+formatStructured <- function(dataOCC, type, varsOld, varsNew, projection, boundary) {
 
-formatStructured <- function(dataOCC, type, varsOld, varsNew,
-                             coordinates, projection, boundary) {
+  #varsOld <- unlist(varsOld)
+  #varsNew <- unlist(varsNew)
 
-  varsOld <- unlist(varsOld)
-  varsNew <- unlist(varsNew)
+  if (inherits(dataOCC, 'Spatial')) dataOCC <- as(dataOCC, 'sf')
 
-  if (inherits(data, 'Spatial')) dataOCC <- as(dataOCC, 'sf')
+  if (!inherits(dataOCC, 'sf')) {
 
-  if (inherits(data, 'data.frame')) {
+    dataOCC <- as.data.frame(dataOCC)
 
-    if (nrow (dataOCC) <= 2) dataOCC$.__PORESP.__ <- 1
+    if (ncol(dataOCC) <= 2) dataOCC$.__PORESP.__ <- 1
 
-    dataOCC <- st_as_sf(x = dataOCC[, c('x', 'y', names(dataOCC)[names(dataOCC) %in% varsOld])],
-                    coords = coordinates,
+    colnames(dataOCC)[names(dataOCC) %in% varsOld$coordinates] <-  c('Longitude', 'Latitude')
+
+    dataOCC <- st_as_sf(x = dataOCC[, c('Longitude', 'Latitude', names(dataOCC)[names(dataOCC) %in% unlist(varsOld)])],
+                    coords = c('Longitude', 'Latitude'),
                     crs = projection)
-
   }
 
   initRows <- nrow(dataOCC)
 
+  st_geometry(dataOCC) <- 'geometry'
+  dataOCC <- st_transform(dataOCC, as.character(projection))
+
   namesData <- colnames(dataOCC)[!colnames(dataOCC) %in% c('geometry', '.__PORESP.__')]
 
-  oldNames <- varsOld[match(namesData, varsOld)]
-  newNames <- varsNew[match(names(oldNames), names(varsNew))]
+  oldNames <- unlist(varsOld)[match(namesData, unlist(varsOld))]
+  newNames <- unlist(varsNew)[match(names(oldNames), names(unlist(varsNew)))]
 
   colnames(dataOCC)[!colnames(dataOCC) %in% c('geometry', '.__PORESP.__')] <- newNames
 
   dataOCC <- dataOCC[unlist(st_intersects(boundary, dataOCC)),]
 
   if (nrow(dataOCC) == 0) stop('Dataset provided has no reccords over the boundary.')
-  if (initRows < nrow(dataOCC)) warning('Some of the records provided are not over the boundary, and will therefore be removed.')
+  if (initRows > nrow(dataOCC)) warning('Some of the records provided are not over the boundary, and will therefore be removed.')
 
   dataOCC
 
