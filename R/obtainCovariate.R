7ab4c8c1fbf685213e37b49b6c1dcb73c14b1809
Philip Mostert <philip.s.mostert@ntnu.no>
2023-06-01 11:39:28 +0200

    First commit: obtainCovariate

--- R/obtainCovariate.R
+++ R/obtainCovariate.R
@@ -1,11 +1,15 @@
 #' @description
-#' Function to obtain covariate layers for a study area
+#' Function to obtain covariate layers from _WorldClim_ around a specified area.
 #' @param covariates A vector of covariate names to obtain.
 #' @param countries A vector of country names to obtain covariates for.
 #' @param projection Coordinate reference system to use in analysis.
+#'
+#' @return A \code{spatialRaster} object of the covariates across the specified area.
 
 obtainCovariate <- function(covariates, countries,
                             projection, path) {
+
+  ##Do for other covariate layers
 
   covariateLayers <- geodata::worldclim_country(country = countries,
                                                 var = covariates,
