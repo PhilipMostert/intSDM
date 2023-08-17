04229597730819d3131752972a6852ace629e33f
Philip Mostert <philip.s.mostert@ntnu.no>
2023-08-17 09:40:10 +0200

    Remove info on PA_redlist data

--- R/data.PA_redlist.rda.R
+++
@@ -1,6 +0,0 @@
-#' A dataset containing the presences and absences of three species: "Fraxinus excelsior", "Ulmus glabra" and "Arnica montana"
-#' @name PA_redlist
-#' @docType data
-#' @keywords data
-#' @references \url{https://www.gbif.org}
-NULL
